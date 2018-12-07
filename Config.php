9df4324dcb15da0b2fe17973fd3b7bf605047e81
Alex Gusev <alex@flancer64.com>
2018-12-07 18:36:00 +0200

    init

---
+++ Config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Container for module's constants (hardcoded configuration).
+ *
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2018
+ */
+
+namespace Flancer32\Base;
+
+class Config
+{
+    /** This module name. */
+    const MODULE = self::MODULE_VENDOR . '_' . self::MODULE_PACKAGE;
+    const MODULE_PACKAGE = 'Base';
+    const MODULE_VENDOR = 'Flancer32';
+
+}
