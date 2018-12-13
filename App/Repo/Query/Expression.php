edaf34e079a026be327a935d45ca2b2356ee3137
Alex Gusev <alex@flancer64.com>
2018-12-13 15:02:00 +0200

    2.3.x refactoring

---
+++ App/Repo/Query/Expression.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2018
+ */
+
+namespace Flancer32\Base\App\Repo\Query;
+
+use Magento\Rule\Model\Condition\Sql\Expression as BaseExpression;
+
+/**
+ * Base module wrapper for Magento SQL expressions.
+ */
+class Expression extends BaseExpression
+{
+
+}
